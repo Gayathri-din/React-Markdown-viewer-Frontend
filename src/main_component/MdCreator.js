87b85181bd5187791dd7a6a9647772e118c0ce5a
Gayathri-din <gayu.abgl@gmail.com>
2024-04-12 13:14:54 +0530

    correction

--- src/main_component/MdCreator.js
+++ src/main_component/MdCreator.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import ReactMarkdown from "react-markdown";
 import UserContext from "../context/UserContext";
 import Navbar from "./Navbar";
